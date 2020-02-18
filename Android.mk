b83865a8ae197296f7c836bed96ffd28ed499da9
jmpfbmx <jmpf.bmx@gmail.com>
2020-02-18 22:16:18 +0100

    pine: Restructuring Android.mk

--- Android.mk
+++ Android.mk
@@ -16,6 +16,12 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),pine)
-
-endif
+include $(CLEAR_VARS)
+LOCAL_MODULE := libion_mtk
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libion_mtk.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libion_mtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
